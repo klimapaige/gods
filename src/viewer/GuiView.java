8927ee0e3ea34d3b324c3fc34a2ea068e6628f77
Matthew Maag <maag.matthew@outlook.com>
2018-02-26 14:42:04 -0700

    Added GUI Classes

--- src/viewer/GuiView.java
+++ src/viewer/GuiView.java
@@ -1,5 +1,30 @@
 package viewer;
 
-public class GuiView {
+import card.Card;
+import player.Player;
+
+public class GuiView implements View{
+	
+	@Override
+	public void showCard(Card card) {
+		
+	}
+	
+	@Override
+	public void showStore() {
+		
+	}
+	
+	@Override
+	public void reviewHand(Player player) {
+			
+	}
+	
+	@Override
+	public int displayMenu() {
+		int option = 0;
+		
+		return option;
+	}
 
 }
--- src/viewer/View.java
+++ src/viewer/View.java
@@ -1,5 +1,13 @@
 package viewer;
 
-public class View {
+import card.Card;
+import player.Player;
 
+public interface View {
+	
+	public void showCard(Card card);
+	public void showStore();
+	public void reviewHand(Player player);
+	public int displayMenu();
+	
 }
