6cc043f140d289d0940dc242065d083b6981ca3f
Paige <klima.paige@gmail.com>
2018-02-27 14:07:45 -0700

    I commentted out driver and change Match.

--- src/controllers/Driver.java
+++ src/controllers/Driver.java
@@ -3,7 +3,7 @@ package controllers;
 public class Driver {
 	
 	public static void main(String[] args) {
-		Game game = new Game();
-		game.mainMenu();
+//		Game game = new Game();
+//		game.mainMenu();
 	}
 }
