19f618f15dd49116531ccca0b92c118f6f26882d
Paige <klima.paige@gmail.com>
2018-02-27 14:26:17 -0700

    Fixed Driver

--- src/controllers/Driver.java
+++ src/controllers/Driver.java
@@ -5,7 +5,7 @@ import viewer.GuiView;
 public class Driver {
 	
 	public static void main(String[] args) {
-//		Game game = new Game();
-//		game.mainMenu();
+		Game game = new Game(new GuiView(), args);
+		game.mainMenu();
 	}
 }
