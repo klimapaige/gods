d08fc0201eb5d2dcfcaa154d127d2e75b367b378
Matthew Maag <maag.matthew@outlook.com>
2018-03-02 14:32:12 -0700

    no message

--- src/card/Creature.java
+++ src/card/Creature.java
@@ -1,16 +1,15 @@
 package card;
 
 import enums.CardRank;
-import player.Player;
 
 public class Creature extends Card {
 
 	private static final long serialVersionUID = -1604270793646181661L;
-	private int sleep;
+	private boolean sleep;
 	private final int MAX_HEALTH;
 	private int health;
 
-	public Creature(String name, CardRank rank, int cost, int manaCost, int power, int powerID, int health, int sleep,
+	public Creature(String name, CardRank rank, int cost, int manaCost, int power, int powerID, int health, boolean sleep,
 			String description) {
 		super(name, rank, cost, manaCost, power, powerID, description);
 		MAX_HEALTH = health;
@@ -22,11 +21,11 @@ public class Creature extends Card {
 		return MAX_HEALTH;
 	}
 
-	public int getSleep() {
+	public boolean getSleep() {
 		return sleep;
 	}
 
-	public void setSleep(int sleep) {
+	public void setSleep(boolean sleep) {
 		this.sleep = sleep;
 	}
 
